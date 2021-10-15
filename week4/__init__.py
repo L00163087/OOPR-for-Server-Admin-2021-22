5dc219660191c4c70242606336994e2ea3f38f00
L00163087 <L00163087@student.lyit.ie>
2021-10-16 01:46:05 +0530

    full Solution of question2

---
+++ week4/__init__.py
@@ -0,0 +1,32 @@
+"""
+# 
+# File          : __init__.py.py
+# Created       : 27-09-2021 02:24 PM
+# Author        : Varnit Rohilla
+# Version       : v1.0.0
+# Licensing     : (c) 2021 Varn R., LYIT
+#                  Avaialable under GNU license Policy
+#             
+"""
+if __name__ == '__main__':
+    '''
+
+   Main method of application 
+
+
+
+   Linear programming only presented here wrt demo of lists
+
+
+
+   Parameters:
+
+    none
+
+
+
+   Returns:
+
+    none
+
+  '''
